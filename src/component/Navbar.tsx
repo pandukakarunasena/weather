e519c356f416ab0ab1ee320b43b45b0ac85fb21f
pandukakarunasena <panduka.karunasena@gmail.com>
2024-02-05 00:44:30 +0530

    Initial commit

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+export default function Navbar({} : Props) {
+    return (
+        <nav>
+            <div>
+                <p>
+                    <h2>Weather</h2>
+                </p>
+            </div>
+        </nav>
+    )
+}
